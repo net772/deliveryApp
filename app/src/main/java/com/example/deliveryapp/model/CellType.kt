cc26b7d8befe33978cf44d37d2b4bff872fa9b20
donghyun <kdhyun000@naver.com>
2022-02-08 17:14:23 +0900

    리스트어뎁터 생성 및 모델 생성

---
+++ app/src/main/java/com/example/deliveryapp/model/CellType.kt
@@ -0,0 +1,10 @@
+package com.example.deliveryapp.model
+
+enum class CellType {
+    RESTAURANT_CELL,
+    LIKE_RESTAURANT_CELL,
+    FOOD_CELL,
+    ORDER_FOOD_CELL,
+    REVIEW_CELL,
+    ORDER_CELL
+}
---
+++ app/src/main/java/com/example/deliveryapp/widget/adapter/ModelRecyclerAdapter.kt
@@ -0,0 +1,9 @@
+package com.example.deliveryapp.widget.adapter
+
+import android.graphics.ColorSpace
+import com.example.deliveryapp.model.Model
+import com.example.deliveryapp.screen.base.BaseViewModel
+
+class ModelRecyclerAdapter<M : Model, VM: BaseViewModel>() {
+
+}
